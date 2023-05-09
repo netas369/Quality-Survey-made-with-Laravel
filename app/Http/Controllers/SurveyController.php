a382308627c9c4f83489a4bf6ff8ad0e090138d0
Xander Smakman <smak0001@hz.nl>
2023-05-09 11:34:42 +0200

    Added new store objects

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -37,11 +37,18 @@ class SurveyController extends Controller
         $survey = new Survey();
 
         $survey->PeopleOnBoard = $request->input('PeopleOnBoard');
-        $survey->AdultsOnBoard = $request->input('AdultsOnBoard');
-        $survey->AgeOfChildren = $request->input('AgeOfChildren');
         $survey->TypeOfVessel = $request->input('TypeOfVessel');
         $survey->FirstVisit = $request->input('FirstVisit');
         $survey->HearAboutHarbour = $request->input('HearAboutHarbour');
+        $survey->OverallCleanliness = $request->input('OverallCleanliness');
+        $survey->StaffFriendlyAndHelpful = $request->input('StaffFriendlyAndHelpful');
+        $survey->SafetyAtTheHarbour = $request->input('SafetyAtTheHarbour');
+        $survey->HowWouldYouRecommendToOthers = $request->input('HowWouldYouRecommendToOthers');
+        $survey->QualityForMoney = $request->input('QualityForMoney');
+        $survey->AnyAdditionalAmenitiesYouWouldLikeToSee = $request->input('AnyAdditionalAmenitiesYouWouldLikeToSee');
+        $survey->DidYouHadAnyIssuesWithTheDocking = $request->input('DidYouHadAnyIssuesWithTheDocking');
+        $survey->WouldYouConsiderReturningToHarbour = $request->input('WouldYouConsiderReturningToHarbour');
+
 
         $survey->save();
         return view('survey.thanks-eng');
