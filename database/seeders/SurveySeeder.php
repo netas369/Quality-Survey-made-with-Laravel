88ef6260e5812c75e91304a56419aa32718d3cc0
netas369 <neve0023@hz.nl>
2023-05-09 14:38:58 +0200

    Added reviews with pagination

---
+++ database/seeders/SurveySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Survey;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SurveySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Survey::factory()
+            ->count(10)
+            ->create();
+    }
+}
---
+++ resources/views/dashboard/show.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>VVW Schelde Flushing</title>
+    <link rel="stylesheet" type="text/css" href="/css/styling.css">
+    <link rel="stylesheet" type="text/css" href="/css/stylingsurvey.css">
+    <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
+</head>
+<body>
+<header>
+    <h1>VVW Schelde Flushing</h1>
+    <h1>Control Dashboard</h1>
+    <nav class="topnav">
+        <ul>
+            <li><a class="{{ Request::path() === '/' ? 'active' : '' }}" href="{{ url('/') }}">Home</a></li>
+            <li><a class="{{ Request::path() === 'survey' ? 'active' : '' }}" href="{{ url('/survey') }}">Survey</a></li>
+            <li><a class="{{ Request::path() === 'dashboard' ? 'active' : '' }}" href="{{ url('/login') }}">Dashboard</a></li>
+        </ul>
+    </nav>
+</header>
+<nav>
+    <div class="sidenav">
+        <h2>MENU</h2>
+        <a href={{ url('/dashboard') }}>Status</a>
+        <a href={{ url('/reviews') }}>Reviews</a>
+    </div>
+</nav>
+<main>
+            <p>Question nr...</p>
+            <p>{{ $review->id }}</p>
+
+</main>
+</body>
+</html>
