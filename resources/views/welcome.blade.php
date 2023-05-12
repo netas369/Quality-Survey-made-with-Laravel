0ecb2679f2c5e17a851ee41bb1ade14abfc3fdcd
Xander Smakman <smak0001@hz.nl>
2023-05-12 09:18:20 +0200

    Updated styling homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,152 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VW Schelde Marina Survey</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <title>VVW Schelde Marina Survey</title>
+    <link rel="stylesheet" href="/css/WelcomePage.css">
 </head>
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
 
-    body {
-        font-family: 'Poppins', sans-serif;
-        margin: 0;
-        background-color: #f2f2f2;
-    }
-
-    header {
-        background-color: #fff;
-        padding: 20px;
-        text-align: center;
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    }
-
-    main {
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 60px 40px;
-        text-align: center;
-    }
-
-    h1 {
-        font-size: 36px;
-        margin-bottom: 20px;
-        color: #005f9e;
-    }
-
-    p {
-        font-size: 18px;
-        margin-bottom: 40px;
-        color: #777;
-        line-height: 1.5;
-    }
-
-    button {
-        background-color: #0072c6;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        padding: 12px 24px;
-        font-size: 18px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
-
-    button:hover {
-        background-color: #005f9e;
-    }
-
-    footer {
-        background-color: #fff;
-        padding: 40px 20px;
-        text-align: center;
-        box-shadow: 0 -2px;
-    (0, 0, 0, 0.1);
-    }
-
-    .footer-nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20px;
-    }
-
-    nav ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-    }
-
-    nav li {
-        margin-right: 20px;
-    }
-
-    nav li:last-child {
-        margin-right: 0;
-    }
-
-    nav a {
-        text-decoration: none;
-        color: #0072c6;
-        font-size: 16px;
-        transition: color 0.3s ease;
-    }
-
-    nav a:hover {
-        color: #005f9e;
-    }
-
-    .social-media-icons {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .social-media-icons a {
-        display: block;
-        width: 40px;
-        height: 40px;
-        background-color: #0072c6;
-        color: #fff;
-        border-radius: 50%;
-        margin-right: 10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 20px;
-        transition: background-color 0.3s ease;
-    }
-
-    .social-media-icons a:hover {
-        background-color: #005f9e;
-    }
-
-    .copy-right {
-        font-size: 14px;
-        color: #777;
-        margin: 0;
-        padding: 20px 0;
-    }
-
-
-
-</style>
 <body>
 <header>
-    <img src="https://ibb.co/pr8YQPv" alt="VW Schelde Marina Logo">
+    <img class="logo-schelde" src="/images/schelde_logo.jpg" alt="VVW Schelde Marina Logo">
 </header>
 <main>
-    <h1>Share Your Experience with VW Schelde Marina</h1>
-    <p>Thank you for choosing VW Schelde Marina. We strive to provide the best possible experience for our customers, and your feedback helps us achieve that goal. Please take a few minutes to complete this survey and share your thoughts with us.</p>
+    <h1>Share Your Experience with VVW Schelde Marina</h1>
+    <p>Thank you for choosing V.V.W. Schelde Marina. We strive to provide the best possible experience for our customers, and your feedback helps us achieve that goal. Please take a few minutes to complete this survey and share your thoughts with us.</p>
     <a href="{{ url('/survey') }}"><button>Start Survey</button><a/>
 </main>
 <footer>
 
-    <ul class="social-media-icons">
-        <li><a href="#"><img src="facebook-icon.png" alt="Facebook Icon"></a></li>
-        <li><a href="#"><img src="twitter-icon.png" alt="Twitter Icon"></a></li>
-        <li><a href="#"><img src="instagram-icon.png" alt="Instagram Icon"></a></li>
-    </ul>
+{{--    <ul class="social-media-icons">--}}
+{{--        <li><a href="https://www.facebook.com/vvwschelde"><img src="facebook-icon.png" alt="Facebook Icon"></a></li>--}}
+{{--        <li><a href="#"><img src="twitter-icon.png" alt="Twitter Icon"></a></li>--}}
+{{--        <li><a href="#"><img src="instagram-icon.png" alt="Instagram Icon"></a></li>--}}
+{{--    </ul>--}}
 </footer>
 </body>
 </html>
