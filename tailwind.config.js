6829e2293f1b70a522323455c14afd726849f8b8
netas369 <neve0023@hz.nl>
2023-05-15 11:30:51 +0200

    Added new dashboard with library

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./resources/views/dashboard/index.blade.php",
+      "./resources/views/dashboard/reviews.blade.php",
+      "./resources/views/dashboard/show.blade.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
