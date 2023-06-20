84750db70ec66d41ea4eee1b54ee5d0065fbebe8
deme0006 <deme0006@hz.nl>
2023-06-21 00:57:36 +0200

    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -57,7 +57,7 @@
                         <li class="mr-3 flex-1">
                             <a href="{{ url('/survey') }}"
                                class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                                <i class="fas fa-layer-group pr-0 md:pr-3"></i><span
+                                <i class="fas fa-pen pr-0 md:pr-3"></i><span
                                     class="pb-1 md:pb-0 text-xs md:text-base text-gray-400 md:text-gray-200 block md:inline-block">Edit survey</span>
                             </a>
                         </li>
